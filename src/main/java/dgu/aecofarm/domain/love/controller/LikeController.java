3e4464aa0c4bfefb654b3def82943f3239ee58d8
YeahOut <pt840072@gmail.com>
2024-07-11 06:22:40 +0900

    feat: LikeController 구현

---
+++ src/main/java/dgu/aecofarm/domain/love/controller/LikeController.java
@@ -0,0 +1,29 @@
+package dgu.aecofarm.domain.love.controller;
+
+import dgu.aecofarm.dto.like.AddLikeDTO;
+import dgu.aecofarm.entity.Response;
+import dgu.aecofarm.domain.love.service.LikeService;
+import dgu.aecofarm.util.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/like")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PatchMapping("/add")
+    public Response<?> addLike(@RequestHeader("Authorization") String authorization, @RequestBody AddLikeDTO addLikeDTO) {
+        Long memberId = extractMemberIdFromToken(authorization);
+        likeService.addLike(memberId, addLikeDTO);
+        return Response.success("좋아요 누르기에 성공하었습니다.");
+    }
+
+    private Long extractMemberIdFromToken(String token) {
+        String jwtToken = token.substring(7);  // "Bearer " 부분을 제거하고 토큰만 추출
+        String memberId = JwtTokenUtil.getLoginId(jwtToken);
+        return Long.valueOf(memberId);  // 추출한 memberId를 Long 타입으로 변환
+    }
+}
