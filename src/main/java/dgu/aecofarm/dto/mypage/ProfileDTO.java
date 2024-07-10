9ff8485296a3402ba27c1d85cdd74cbdfa74e934
haeun1107 <haeun9394@gmail.com>
2024-07-11 00:29:42 +0900

    feat: setter -> builder 변경

--- src/main/java/dgu/aecofarm/dto/mypage/ProfileDTO.java
+++ src/main/java/dgu/aecofarm/dto/mypage/ProfileDTO.java
@@ -1,10 +1,12 @@
 package dgu.aecofarm.dto.mypage;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class ProfileDTO {
     private String userName;
     private String email;
