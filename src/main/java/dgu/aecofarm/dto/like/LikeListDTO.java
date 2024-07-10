d1918641ee4f852cf2c873dc004490b9941a8951
YeahOut <pt840072@gmail.com>
2024-07-11 08:02:37 +0900

    feat: 좋아요 내역 DTO 생성

---
+++ src/main/java/dgu/aecofarm/dto/like/LikeListDTO.java
@@ -0,0 +1,36 @@
+package dgu.aecofarm.dto.like;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikeListDTO {
+    private List<LendingItem> lendingItems;
+    private List<BorrowingItem> borrowingItems;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LendingItem {
+        private Long contractId;
+        private String itemName;
+        private int price;
+        private int time;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class BorrowingItem {
+        private Long contractId;
+        private String itemName;
+        private String itemImage;
+        private int price;
+        private int time;
+    }
+}
