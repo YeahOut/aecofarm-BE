1776cbfdc5bbb635be315cc12c72b6bf6a668cc4
YeahOut <pt840072@gmail.com>
2024-07-11 07:00:18 +0900

    feat: DTO 구조 수정

--- src/main/java/dgu/aecofarm/dto/like/AddLikeDTO.java
+++ src/main/java/dgu/aecofarm/dto/like/AddLikeDTO.java
@@ -1,10 +1,10 @@
 package dgu.aecofarm.dto.like;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class AddLikeDTO {
     private Long itemId;
 }
