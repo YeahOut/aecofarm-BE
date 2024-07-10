8bc15b8e10db1f6009d5017530935c08dc3642d4
YeahOut <pt840072@gmail.com>
2024-07-10 14:07:33 +0900

    feat: HistoryDTO 추가

---
+++ src/main/java/dgu/aecofarm/dto/mypage/HistoryDTO.java
@@ -0,0 +1,11 @@
+package dgu.aecofarm.dto.mypage;
+
+public class HistoryDTO {
+    private Long contractId;
+    private String itemName;
+    private String itemImage;
+    private int time;
+    private int price;
+    private Boolean likeStatus;
+    
+}
