137041ee79bec59b54e56367ced7fac42fa862c5
YeahOut <pt840072@gmail.com>
2024-07-10 15:17:03 +0900

    fix: 프로필 업데이트를 위한 Member클래스 수정

--- src/main/java/dgu/aecofarm/domain/mypage/service/MyPageService.java
+++ src/main/java/dgu/aecofarm/domain/mypage/service/MyPageService.java
@@ -1,7 +1,8 @@
 package dgu.aecofarm.domain.mypage.service;
 
-import dgu.aecofarm.dto.mypage.MyPageResponseDTO;
+import dgu.aecofarm.dto.mypage.*;
 
 public interface MyPageService {
     MyPageResponseDTO getMyPage(Long memberId);
+    void updateProfile(Long memberId, UpdateProfileDTO updateProfileDTO);
 }
