9a1c19c820167436bd4d8d435319769633db8f80
YeahOut <pt840072@gmail.com>
2024-07-11 09:03:21 +0900

    feat: MyPageList조회 DTO 생성

---
+++ src/main/java/dgu/aecofarm/dto/mypage/MyPageContractsDTO.java
@@ -0,0 +1,50 @@
+package dgu.aecofarm.dto.mypage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyPageContractsDTO {
+    private List<LendingItem> lendingItems;
+    private List<BorrowingItem> borrowingItems;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LendingItem {
+        private Long contractId;
+        private String itemName;
+        private int price;
+        private String itemPlace;
+        private int time;
+        private int contractTime;
+        private List<String> itemHash;
+        private boolean likeStatus;
+        private boolean donateStatus;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class BorrowingItem {
+        private Long contractId;
+        private String itemName;
+        private String itemImage;
+        private int price;
+        private String itemPlace;
+        private int time;
+        private int contractTime;
+        private List<String> itemHash;
+        private boolean likeStatus;
+        private boolean donateStatus;
+    }
+}
