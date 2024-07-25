74bce9b265194426e50f8af1f0f223c2fe4a003e
haeun1107 <haeun9394@gmail.com>
2024-07-25 14:50:38 +0900

    refactor: cors 수정

--- src/main/java/dgu/aecofarm/config/WebConfig.java
+++ src/main/java/dgu/aecofarm/config/WebConfig.java
@@ -14,10 +14,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://aecofarm-fe-6pnz.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
