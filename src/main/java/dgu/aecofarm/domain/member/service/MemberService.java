c75dbf201b454fa8ad186c64e9862f6a8e26a749
YeahOut <pt840072@gmail.com>
2024-08-02 10:46:35 +0900

    fix: signup interface 변경

--- src/main/java/dgu/aecofarm/domain/member/service/MemberService.java
+++ src/main/java/dgu/aecofarm/domain/member/service/MemberService.java
@@ -7,7 +7,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Optional;
 
 public interface MemberService {
-    String signup(SignupRequestDTO registerRequestDTO, String imageUrl);
+    String initiateSignup(SignupRequestDTO signupRequestDTO, String imageUrl);
+
+    String completeSignup(SignupRequestDTO signupRequestDTO, String authCode, String expectedCode, String imageUrl);
 
     String uploadFile(MultipartFile file);
 
